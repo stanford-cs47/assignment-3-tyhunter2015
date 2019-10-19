7536559659ac13776b8822ae61556974cb662ecf
Ty Hunter <tyhunter2015@gmail.com>
2019-10-19 12:15:09 -0700

    furthest (sat morning)

--- App.js
+++ App.js
@@ -8,9 +8,20 @@
 */
 
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import { Images, Colors } from './App/Themes'
 import APIRequest from './App/Config/APIRequest'
+import { Platform } from '@unimodules/core';
 
 import News from './App/Components/News'
 import Search from './App/Components/Search'
@@ -25,9 +36,8 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-
     //uncomment this to run an API query!
-    //this.loadArticles();
+    this.loadArticles();
   }
 
   async loadArticles(searchTerm = '', category = '') {
@@ -42,19 +52,81 @@ export default class App extends React.Component {
     this.setState({loading: false, articles: resultArticles})
   }
 
+  getArticleContent = () => {
+    const {articles, loading} = this.state;
+
+    let contentDisplayed = null;
+
+    if (loading) {
+      contentDisplayed = <ActivityIndicator
+                            style={styles.ActivityIndicator}
+                            size="large" color="black"/>;
+    } else {
+      contentDisplayed = <News articles={articles}/>;
+    }
+
+    return (
+      <View style={{flex:1}}>
+        {contentDisplayed}
+      </View>
+    )
+  }
+
+  onChangeText = searchText => {
+    this.setState({searchText});
+  }
+
+  //not sure if needed
+  renderArticle = (index, item) => (
+    <News
+      text={item}
+      deleteTodo={() => this.deleteTodo(index)}
+    />
+    <Text>text</Text>
+  )
+//still not sure
+  keyExtractor = index => {
+    return index.toString();
+  }
+
   render() {
     const {articles, loading} = this.state;
 
     return (
       <SafeAreaView style={styles.container}>
 
-        <Text style={{textAlign: 'center'}}>Have fun! :) {"\n"} Start by changing the API Key in "./App/Config/AppConfig.js" {"\n"} Then, take a look at the following components: {"\n"} NavigationButtons {"\n"} Search {"\n"} News {"\n"} 🔥</Text>
+        <Image
+            source = {require('./App/Images/nyt.png')}
+            style = {{
+              width: '90%',
+              height: '90%',
+              resizeMode: 'contain',
+              height: 72,
+            }}
+        />
 
-        {/*First, you'll need a logo*/}
+        <TextInput
+            style={styles.textinput}
+            onChangeText={searchText => this.onChangeText(searchText)}
+            value={this.state.searchText}
+        />
 
-        {/*Then your search bar*/}
+        {}
 
-        {/*And some news*/}
+        <View style={styles.flatlist}>
+          <FlatList
+            data={this.state.articles}
+            //renderItem={{articles}}
+            renderItem={({ index, item }) => this.renderArticle(index, item)}
+            /*renderItem={( { item } ) => 
+              <ToDo text={item}/>
+            }*/
+            keyExtractor={(item, index) =>
+              return this.getArticleContent().toString();
+              this.keyExtractor(index)
+            }
+          />
+        </View>
 
         {/*Though, you can style and organize these however you want! power to you 😎*/}
 
@@ -71,5 +143,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  textinput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  flatlist: {
+    flex: 1,
+    width: "100%"
   }
 });
